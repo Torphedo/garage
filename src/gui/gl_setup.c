78b9baa65c5e9090d08cfb483f3b27d4f440c842
torph <73564623+Torphedo@users.noreply.github.com>
2024-05-23 22:18:57 -0400

    Minor MacOS compatability changes This is untested.

--- src/gui/gl_setup.c
+++ src/gui/gl_setup.c
@@ -11,7 +11,10 @@
 #include "gl_debug.h"
 
 void frame_resize_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
+    int framebuf_width = 0;
+    int framebuf_height = 0;
+    glfwGetFramebufferSize(window, &framebuf_width, &framebuf_height);
+    glViewport(0, 0, framebuf_width, framebuf_height);
 }
 
 void glfw_error(int err_code, const char* msg) {
@@ -36,9 +39,10 @@ GLFWwindow* setup_opengl(s32 width, s32 height, const char* window_name, bool en
     // Use OpenGL Core 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    // Request a debug context as determined by caller
+    // Request a debug context if requested by caller
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, enable_debug);
     
     GLFWwindow* window = glfwCreateWindow(width, height, window_name, NULL, NULL);
