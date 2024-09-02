107be86f4854243fbb51776a9a712720e68221a2
torph <73564623+Torphedo@users.noreply.github.com>
2024-09-01 21:54:44 -0400

    Fix Zig Win32 build

--- build_win32_zig.sh
+++ build_win32_zig.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-zig cc -target x86_64-windows -O2 src/*.c src/common/*.c src/editor/*.c ext/stb_*.c ext/glad/src/*.c ext/glfw/src/*.c -Iext/cglm/include -Iext/glad/include -Iext/glfw/include -Isrc -Iext -DGLFW_BUILD_WIN32 -D_GLFW_WIN32 -lgdi32 -ogarage.exe
+zig cc -target x86_64-windows -O2 src/*.c src/common/*.c src/editor/*.c ext/stb_*.c ext/glad/src/*.c ext/glfw/src/*.c ext/physfs/src/*.c  -Iext/cglm/include -Iext/glad/include -Iext/glfw/include -Iext/physfs/src -Isrc -Iext -DGLFW_BUILD_WIN32 -D_GLFW_WIN32 -lgdi32 -ogarage.exe
 
